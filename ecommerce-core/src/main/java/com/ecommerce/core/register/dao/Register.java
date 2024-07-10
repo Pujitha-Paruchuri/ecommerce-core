c65e99d9d7a00e910d09a34bf52487d853a437d1
Pujitha-Paruchuri <paruchuri.puji@gmail.com>
2024-07-10 17:54:09 -0400

    Code changes register dao and repo.

--- ecommerce-core/src/main/java/com/ecommerce/core/register/dao/Register.java
+++ ecommerce-core/src/main/java/com/ecommerce/core/register/dao/Register.java
@@ -1,5 +1,70 @@
 package com.ecommerce.core.register.dao;
 
 public class Register {
-
+	
+	Integer Id;
+	String firstName;
+	String lastName;
+	String email;
+	String address;
+	String city;
+	String state;
+	Integer zipCode;
+	String favCuisine;
+	
+	public Integer getId() {
+		return Id;
+	}
+	public void setId(Integer id) {
+		Id = id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public Integer getZipCode() {
+		return zipCode;
+	}
+	public void setZipCode(Integer zipCode) {
+		this.zipCode = zipCode;
+	}
+	public String getFavCuisine() {
+		return favCuisine;
+	}
+	public void setFavCuisine(String favCuisine) {
+		this.favCuisine = favCuisine;
+	}
+	
 }
--- ecommerce-core/src/main/java/com/ecommerce/core/register/repository/RegisterRepository.java
+++ ecommerce-core/src/main/java/com/ecommerce/core/register/repository/RegisterRepository.java
@@ -1,5 +1,10 @@
 package com.ecommerce.core.register.repository;
 
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class RegisterRepository {
 
+
+	
 }
